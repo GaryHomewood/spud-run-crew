37e3c162aa35b09b6d3a573c6d384494c58d258e
Gary Homewood <gary@potatolondon.com>
2023-11-24 10:56:02 +0000

    fix: change CSV parser

--- components/dashboard.js
+++ components/dashboard.js
@@ -1,19 +1,20 @@
 import { AttendanceGraph } from './attendanceGraph';
 import { RankingGraph } from './rankingGraph';
 import { PaceGraph } from './paceGraph';
+import Papa from 'papaparse'
 
 class Dashboard {
   init() {
-    Tabletop.init({
-      key: '1MD4emE3qmFyUSM5cFHu8ZqfEaykRIl_ntogntnPfGfs',
-      callback: this.drawGraphs,
-      simpleSheet: true
-    });
+    Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vQPdv3BxseDSRtsZ67azc0jrq649ixFLjhNSqj697Ga045ugmcRfZOoW-_Sw9qhyULx9j8DNrZUv_hl/pub?gid=0&single=true&output=csv', {
+      download: true,
+      header: true,
+      complete: (results) => this.drawGraphs(results.data)
+    })
   }
 
   drawGraphs(data) {
     document.getElementById('count').innerText = data.length;
-    
+
     let dimens = { width: 300, height: 130, top: 30, right: 50, bottom: 40, left: 50 };
 
     const attendanceGraph = new AttendanceGraph(dimens, data);
